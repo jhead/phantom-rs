4f304319b30931c382b28db015343e0d177e28e0
Justin Head <jhead@jxh.io>
2025-06-15 00:19:11 -0700

    remove unused spec

--- app/PhantomApp/specs/NativePhantom.ts
+++
@@ -1,8 +0,0 @@
-import {TurboModule} from 'react-native';
-import {TurboModuleRegistry} from 'react-native';
-
-export interface Spec extends TurboModule {
-  create(opts: string): void;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('NativePhantom');
